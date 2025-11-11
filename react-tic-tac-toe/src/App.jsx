99bfe5fbf53aaf633ac2fafe56172c982b134c02
Chu A. <chu.amk22@gmail.com>
2025-11-11 14:44:18 +0100

    work on form structure
    use yup and formik for building form and form validaition

--- react-tic-tac-toe/src/App.jsx
+++ react-tic-tac-toe/src/App.jsx
@@ -1,52 +1,213 @@
-import { useCallback, useState } from "react";
+// import { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [boxes, setBoxes] = useState(Array(9).fill(null));
+//   const [playerTurn, setPlayerTurn] = useState(true);
+
+//   const checkWinner = (box) => {
+//     const wineLines = [
+//       [0, 1, 2],
+//       [0, 3, 6],
+//       [0, 4, 8],
+//       [1, 4, 7],
+//       [2, 5, 8],
+//       [2, 4, 6],
+//       [3, 4, 5],
+//       [6, 7, 8],
+//     ];
+
+//     for (let i = 0; i < wineLines.length; i++) {
+//       const [a, b, c] = wineLines[i];
+//       if (box[a] && box[a] === box[b] && box[a] === box[c]) return box[a];
+//     }
+//     return null;
+//   };
+
+//   const handleClick = (index) => {
+//     const newBox = boxes.slice();
+//     newBox[index] = playerTurn ? "X" : "O";
+//     setBoxes(newBox);
+//     setPlayerTurn(!playerTurn);
+//   };
+
+//   const winner = checkWinner(boxes);
+//   if (winner)
+//     return (
+//       <>
+//         <p>{winner ? `Winner: ${winner}` : ""}</p>
+//       </>
+//     );
+
+//   return (
+//     <>
+//       <h1>Tic Tac Toe</h1>
+//       <main>
+//         <div className="container">
+//           {boxes.map((value, index) => (
+//             <button key={index} onClick={() => handleClick(index)}>
+//               {value}
+//             </button>
+//           ))}
+//         </div>
+//         <p>{`Next Player: ${playerTurn ? "X" : "O"}`}</p>
+//       </main>
+//     </>
+//   );
+// }
+// export default App;
+
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const boxSizes = 3;
 function App() {
-  const [boxes, setBoxes] = useState(
-    Array.from({ length: 9 }, (_, index) => index + 1).fill(null)
-  );
-  const [playerTurn, setPlayerTurn] = useState(true);
+  const [draw, setDraw] = useState(false);
+  const [winner, setWinner] = useState(null);
 
-  const handleClick = (index) => {
-    const newBox = boxes.slice();
-    newBox[index] = playerTurn ? "X" : "O";
-    setBoxes(newBox);
-    setPlayerTurn(!playerTurn);
-    console.log(newBox);
-  };
+  const [turn, setTurn] = useState("X");
 
-  const checkWinner = (squares) => {
-    const wineLines = [
-      [0, 1, 2],
-      [0, 3, 6],
-      [0, 4, 8],
-      [1, 4, 7],
-      [2, 5, 8],
-      [2, 4, 6],
-      [3, 4, 5],
-      [6, 7, 8],
-    ];
-
-    for (let [a, b, c] of wineLines.length) {
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
-        return squares[a];
+  const [box, setBox] = useState(() => {
+    const xCords = [];
+    for (let i = 0; i < boxSizes; i++) {
+      xCords.push(Array(boxSizes).fill(null));
     }
-    return null;
+    return xCords;
+  });
+
+  const calculateWinner = (board) => {
+    const inferWinnerCoordinates = (board) => {
+      const coordinatesCombinations = [];
+      for (let i = 0; i < boxSizes; i++) {
+        let rowCoordinates = [];
+        let columnCoordinates = [];
+        for (let j = 0; j < boxSizes; j++) {
+          rowCoordinates.push([i, j]);
+          columnCoordinates.push([j, i]);
+        }
+        coordinatesCombinations.push(rowCoordinates);
+        coordinatesCombinations.push(columnCoordinates);
+      }
+
+      let diagonalCoordinates1 = [];
+      let diagonalCoordinates2 = [];
+      for (let i = 0; i < boxSizes; i++) {
+        diagonalCoordinates1.push([i, i]);
+        diagonalCoordinates2.push([i, boxSizes - 1 - i]);
+      }
+      coordinatesCombinations.push(diagonalCoordinates1);
+      coordinatesCombinations.push(diagonalCoordinates2);
+
+      return coordinatesCombinations;
+    };
+
+    const winningCords = inferWinnerCoordinates();
+
+    let hasWon = false;
+    let winner = null;
+
+    for (let cords of winningCords) {
+      const winX = cords.every(([x, y]) => board[x][y] === "X");
+
+      const winO = cords.every(([x, y]) => board[x][y] === "O");
+
+      const win = winX || winO;
+
+      if (win) {
+        hasWon = true;
+        winner = board[cords[0][0]][cords[0][1]];
+        break;
+      }
+    }
+    return hasWon ? winner : null;
   };
+
+  const nextRandomMove = (board) => {
+    const emptyCells = [];
+    for (let i = 0; i < boxSizes; i++) {
+      for (let j = 0; j < boxSizes; j++) {
+        if (board[i][j] === null) {
+          emptyCells.push([i, j]);
+        }
+      }
+    }
+
+    if (emptyCells.length === 0) {
+      return null;
+    }
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    return emptyCells[randomIndex];
+  };
+
+  const play = (x, y) => {
+    if (box[x][y] !== null) {
+      return;
+    }
+
+    // deep copy of the box state
+    const newBox = box.map((row) => row.slice());
+    // assuming we are always playing X and machine always plays O
+    newBox[x][y] = "X";
+    setBox(newBox);
+    const winner = calculateWinner(newBox);
+    if (winner) {
+      setWinner(winner);
+      return;
+    }
+
+    setTurn("O");
+  };
+
+  useEffect(() => {
+    if (turn === "O") {
+      const newBox = box.map((row) => row.slice());
+      setTimeout(() => {
+        const machineMove = nextRandomMove(newBox);
+        if (machineMove) {
+          const [machineX, machineY] = machineMove;
+          newBox[machineX][machineY] = "O";
+          setBox(newBox);
+
+          const winner = calculateWinner(newBox);
+
+          if (winner) {
+            setWinner(winner);
+            return;
+          }
+        } else {
+          setDraw(true);
+        }
+        setTurn("X");
+      }, 1000);
+    }
+  }, [turn]);
 
   return (
-    <>
-      <h1>Tic Tac Toe</h1>
-      <main>
-        <div className="container">
-          {boxes.map((value, index) => (
-            <button key={index} onClick={() => handleClick(index)}>
-              {value}
-            </button>
+    <div className="page">
+      <div className="board-main-container">
+        <h1 className="board-title">Tic Tac Toe Game board</h1>
+        <div className="board-container">
+          <div className="draw-indicator">{draw ? "Game Drawn!" : ""}</div>
+          <div className="winner-indicator">
+            {winner ? `Winner: ${winner}` : ""}
+          </div>
+          {box.map((row, rowIndex) => (
+            <div key={rowIndex} className="board-row">
+              {row.map((cell, cellIndex) => (
+                <div
+                  onClick={() => !winner && !draw && play(rowIndex, cellIndex)}
+                  key={cellIndex}
+                  className="box"
+                >
+                  {cell}
+                </div>
+              ))}
+            </div>
           ))}
         </div>
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
+
 export default App;
---
+++ user-profile-dashboard/src/components/Form.jsx
@@ -0,0 +1,118 @@
+import { useFormik } from "formik";
+import { schema } from "../schemas";
+
+const onSubmit = (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  actions.resetForm();
+};
+
+export default function Form() {
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    errors,
+    touched,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      address: "",
+    },
+    validationSchema: schema,
+    onSubmit,
+  });
+  console.log(values);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <fieldset>
+        <legend>Registration</legend>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          value={values.firstName}
+          onChange={handleChange}
+          id="firstName"
+          type="text"
+          placeholder="First Name"
+          onBlur={handleBlur}
+          className={errors.firstName && touched.firstName ? "input-error" : ""}
+        />
+        {errors.firstName && touched.firstName ? (
+          <p className="error">{errors.firstName}</p>
+        ) : (
+          ""
+        )}
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          value={values.lastName}
+          onChange={handleChange}
+          id="lastName"
+          type="text"
+          placeholder="Last Name"
+          onBlur={handleBlur}
+          className={errors.lastName && touched.lastName ? "input-error" : ""}
+        />
+        {errors.lastName && touched.lastName ? (
+          <p className="error">{errors.lastName}</p>
+        ) : (
+          ""
+        )}
+        <label htmlFor="email">Email</label>
+        <input
+          value={values.email}
+          onChange={handleChange}
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          onBlur={handleBlur}
+          className={errors.email && touched.email ? "input-error" : ""}
+        />
+        {errors.email && touched.email ? (
+          <p className="error">{errors.email}</p>
+        ) : (
+          ""
+        )}
+        <label htmlFor="phone">Phone</label>
+        <input
+          value={values.phone}
+          onChange={handleChange}
+          id="phone"
+          type="number"
+          placeholder="Enter your phone"
+          onBlur={handleBlur}
+          className={errors.phone && touched.phone ? "input-error" : ""}
+        />
+        {errors.phone && touched.phone ? (
+          <p className="error">{errors.phone}</p>
+        ) : (
+          ""
+        )}
+        <label htmlFor="address">Address</label>
+        <input
+          value={values.address}
+          onChange={handleChange}
+          id="address"
+          type="text"
+          placeholder="Enter your address"
+          onBlur={handleBlur}
+          className={errors.address && touched.address ? "input-error" : ""}
+        />
+        {errors.address && touched.address ? (
+          <p className="error">{errors.address}</p>
+        ) : (
+          ""
+        )}
+      </fieldset>
+      <button disabled={isSubmitting} type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
