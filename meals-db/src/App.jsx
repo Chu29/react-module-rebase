7ec0ce62a91330405a07d91dc91e76a8fbc239ec
Chu A. <chu.amk22@gmail.com>
2025-11-12 13:25:39 +0100

    configure routes

--- meals-db/src/App.jsx
+++ meals-db/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import MealDetailsPage from "./pages/MealDetailsPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="" Component={HomePage}></Route>
+        <Route path="/categories" Component={CategoryPage}></Route>
+        <Route path="/meal-details" Component={MealDetailsPage}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- user-profile-dashboard/src/components/Form.jsx
+++ user-profile-dashboard/src/components/Form.jsx
@@ -1,39 +1,30 @@
 import { useFormik } from "formik";
-import { schema } from "../schemas";
-
-const onSubmit = (values, actions) => {
-  console.log(values);
-  console.log(actions);
-  actions.resetForm();
-};
+import { userSchema } from "../schemas/user.schema";
 
 export default function Form() {
-  const {
-    values,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    errors,
-    touched,
-    isSubmitting,
-  } = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      phone: "",
-      address: "",
-    },
-    validationSchema: schema,
-    onSubmit,
-  });
-  console.log(values);
-  console.log(errors);
+  const onSubmit = (values, actions) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(values));
+    actions.resetForm();
+  };
+  const LOCAL_STORAGE_KEY = "userProfile";
+
+  const { values, handleChange, handleSubmit, errors, touched, isSubmitting } =
+    useFormik({
+      initialValues: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        address: "",
+      },
+      validationSchema: userSchema,
+      onSubmit,
+    });
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit} className="form">
-      <h2>Create Profile</h2>
+        <h2>Create Profile</h2>
         <label htmlFor="firstName">First Name</label>
         <input
           value={values.firstName}
@@ -41,7 +32,6 @@ export default function Form() {
           id="firstName"
           type="text"
           placeholder="First Name"
-          onBlur={handleBlur}
           className={errors.firstName && touched.firstName ? "input-error" : ""}
         />
         {errors.firstName && touched.firstName ? (
@@ -56,7 +46,6 @@ export default function Form() {
           id="lastName"
           type="text"
           placeholder="Last Name"
-          onBlur={handleBlur}
           className={errors.lastName && touched.lastName ? "input-error" : ""}
         />
         {errors.lastName && touched.lastName ? (
@@ -71,7 +60,6 @@ export default function Form() {
           id="email"
           type="email"
           placeholder="Enter your email"
-          onBlur={handleBlur}
           className={errors.email && touched.email ? "input-error" : ""}
         />
         {errors.email && touched.email ? (
@@ -86,7 +74,6 @@ export default function Form() {
           id="phone"
           type="number"
           placeholder="Enter your phone"
-          onBlur={handleBlur}
           className={errors.phone && touched.phone ? "input-error" : ""}
         />
         {errors.phone && touched.phone ? (
@@ -101,7 +88,6 @@ export default function Form() {
           id="address"
           type="text"
           placeholder="Enter your address"
-          onBlur={handleBlur}
           className={errors.address && touched.address ? "input-error" : ""}
         />
         {errors.address && touched.address ? (
