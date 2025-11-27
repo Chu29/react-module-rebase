518f4fd2c67febba834059f7432a97af70bfba8c
Chu A. <chu.amk22@gmail.com>
2025-11-27 11:44:15 +0100

    consuming the store in a component

--- zustand-tut/src/App.jsx
+++ zustand-tut/src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import useBearStore from "./store/bearStore";
+import './index.css'
 
 export default function App() {
+  const { bears, increase } = useBearStore();
+
   return (
-    <div>App</div>
-  )
+    <div className="container">
+      <h1>{bears} bears around hear...</h1>
+      <button onClick={increase}>Add one</button>
+    </div>
+  );
 }
